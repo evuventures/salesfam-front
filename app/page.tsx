41ea9fe9749d4e94ddc1f97931c40d4af94ec166
mph7 <matheus.philippe2002@gmail.com>
2026-08-25 09:40:42 -0300

    feat: implement ValueProps section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/sections/Hero";
+import { ValueProps } from "@/components/sections/ValueProps";
 
 export default function Home() {
 	return (
 		<main className="pt-24 md:pt-32 pb-20 space-y-20 md:space-y-32">
 			<Hero />
+			<ValueProps />
 		</main>
 	);
 }
