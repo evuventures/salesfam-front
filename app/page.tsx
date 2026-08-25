433a7c182f71219f5c2138a13f75b3fb21f25b23
mph7 <matheus.philippe2002@gmail.com>
2026-08-25 09:44:19 -0300

    feat: implement HowItWorks section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Hero from "@/components/sections/Hero";
+import { HowItWorks } from "@/components/sections/HowItWorks";
 import { ValueProps } from "@/components/sections/ValueProps";
 
 export default function Home() {
-	return (
-		<main className="pt-24 md:pt-32 pb-20 space-y-20 md:space-y-32">
-			<Hero />
-			<ValueProps />
-		</main>
-	);
+    return (
+        <main className="pt-24 md:pt-32 pb-20 space-y-20 md:space-y-32">
+            <Hero />
+            <ValueProps />
+            <HowItWorks />
+        </main>
+    );
 }
