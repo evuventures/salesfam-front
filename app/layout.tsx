a29f12625a01b619c18773f0f60004d9c66d0f9c
Arman Hossain Atunu <armanhossainatunu@gmail.com>
2026-08-28 05:11:43 +0600

    style: format app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,39 +6,39 @@ import { cn } from "@/lib/utils";
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Welcome to Salesfam",
-  description:
-    "Empowering companies and independent sellers to grow distribution through trust and transparency.",
+	title: "Welcome to Salesfam",
+	description:
+		"Empowering companies and independent sellers to grow distribution through trust and transparency.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: {
-  children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-  return (
-    <html
-      lang="en"
-      className={cn(
-        "h-full",
-        "antialiased",
-        geistSans.variable,
-        geistMono.variable,
-        "font-sans",
-        inter.variable,
-      )}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={cn(
+				"h-full",
+				"antialiased",
+				geistSans.variable,
+				geistMono.variable,
+				"font-sans",
+				inter.variable,
+			)}
+		>
+			<body className="min-h-full flex flex-col">{children}</body>
+		</html>
+	);
 }
